9c58cdd83b40459bfe39d4a8d778de3eb87fdefd
Andrew <pXn.Tilde@protonmail.com>
2022-04-02 03:33:58 -0700

    _cries_

---
+++ game/Interfaces/IMovable.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ldjam50.Interfaces
+{
+    public interface IMovable
+    {
+        public Vector2 Position { get; set; }
+    }
+}
