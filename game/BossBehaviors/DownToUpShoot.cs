d66687b3c1c3ceafec64d8cab2aa3ea5f9b76e31
Michael Lange <dingoeatingfuzz@gmail.com>
2022-04-02 23:06:30 -0700

    Refactor and fix the DownToUpShoot behavior

--- game/BossBehaviors/DownToUpShoot.cs
+++ game/BossBehaviors/DownToUpShoot.cs
@@ -8,22 +8,18 @@ public class DownToUpShoot : Node, IBossBehavior
     public event EventHandler<BossBehaviorDoneArgs> Done;
 
     public void Start(Boss boss) {
-        var shots = 1;
-        boss.Fire(Vector2.Down);
-        GD.Print("shooting");
-        var baseRad = Mathf.Pi / 2;
-        while (shots < 7)
-        {
-            //+ Mathf.Pi / 2 is Left. Divide that shit up
-            var dir = new Vector2(Mathf.Cos(baseRad + (Mathf.Pi / 2)), Mathf.Sin(baseRad + (Mathf.Pi / 2)));
-            Global.Time.AddNotify(shots * 0.1f, () => {
+        var count = 8;
+        var delay = 0.1f;
+        for (int i = 0; i < count; i++) {
+            var ang =  Mathf.Pi / 2 / (count - 1) * i;
+            var dir = Vector2.Down.Rotated(ang);
+            Global.Time.AddNotify(i * delay + delay, () => {
                 boss.Fire(dir);
             });
-            shots++;
         }
-        Global.Time.AddNotify(shots * 0.1f, () => {
-            boss.Fire(Vector2.Left);
-            Done(this, new BossBehaviorDoneArgs(true));
+
+        Global.Time.AddNotify(count * delay + delay, () => {
+            Done(this, new BossBehaviorDoneArgs());
         });
     }
 }
