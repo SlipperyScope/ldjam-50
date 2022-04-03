ec2f47ddc5e4911c46c631b515191c7789f14a7d
Michael Lange <dingoeatingfuzz@gmail.com>
2022-04-03 14:19:37 -0700

    Implment spiral gun

--- game/Entities/SpiralBossGun.cs
+++ game/Entities/SpiralBossGun.cs
@@ -5,9 +5,19 @@ namespace ldjam50.Entities
 {
     public class SpiralBossGun : BossGun
     {
+        const String BulletScenePath = "res://Bullet.tscn";
+        public PackedScene BulletScene;
         public override void Shoot(Vector2 direction)
         {
-            throw new NotImplementedException();
+            var bullet = BulletScene.Instance<Bullet>();
+            GetTree().Root.AddChild(bullet);
+            bullet.Position = this.GlobalPosition;
+            bullet.Velocity = direction * 700;
+        }
+
+        public override void _Ready()
+        {
+            BulletScene = GD.Load<PackedScene>(BulletScenePath);
         }
     }
 }
