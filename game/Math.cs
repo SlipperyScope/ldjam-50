43ae7eb149c315df44bcf7684264998e65d4ed87
Michael Lange <dingoeatingfuzz@gmail.com>
2022-04-03 00:48:23 -0700

    The start of a general math utils class

---
+++ game/Math.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+using ldjam50;
+using System.Collections.Generic;
+
+public static class Math {
+    public static Vector2[] LerpAngle(int count, float start, float end) {
+        // Special case count 1 (which shoots in the middle instead of start)
+        if (count == 1) {
+            return new Vector2[]{ Vector2.Right.Rotated(start + (end - start) / 2) };
+        }
+
+        var angles = new Vector2[count];
+        for (int i = 0; i < count; i++) {
+            angles[i] = Vector2.Right.Rotated(start + (end - start) / (count - 1) * i);
+        }
+        return angles;
+    }
+}
