ab3c3306a8bc752a20e934168ab7167b79fc8bf8
Michael Lange <dingoeatingfuzz@gmail.com>
2022-04-02 17:51:14 -0700

    Mapping system almost works

--- game/BossManager/BossManager.cs
+++ game/BossManager/BossManager.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class BossBehaviorDoneArgs : EventArgs {
     // Placeholder, may be useful, idk, stop asking questions
@@ -14,31 +15,98 @@ public interface IBossBehavior {
     public event EventHandler<BossBehaviorDoneArgs> Done;
 }
 
-public class BossManager : Node
-{
-    public IBossBehavior StartingBehavior;
+public interface IBossBehaviorConfig {
+    public BehaviorMapping Initial { get; }
+}
 
+public static class BossBehaviors {
+    public delegate IBossBehavior Builder();
+
+    static Dictionary<string,Builder> Map = new Dictionary<string, Builder>(){
+        { "Wait", () => new Wait() },
+        { "BoringShoot", () => new BoringShoot() }
+    };
+    public static IBossBehavior MakeA(string name) {
+        if (!Map.ContainsKey(name)) throw new Exception($"Steve pls, there's no bulder for the name '{name}'.");
+        return Map[name]();
+    }
+}
+
+public class BehaviorMapping {
+    public string Name;
+    public BehaviorMapping[] Next;
+    public BehaviorMapping[] Interrupt;
+    public BehaviorMapping(string name, BehaviorMapping[] next, BehaviorMapping[] interrupt) {
+        Name = name;
+        Next = next;
+        Interrupt = interrupt;
+    }
+    public BehaviorMapping(string name, params BehaviorMapping[] next) {
+        Name = name;
+        Next = next;
+    }
+    public BehaviorMapping Any(BehaviorMapping[] behaviors) {
+        var idx = GD.Randi() % behaviors.Length;
+        GD.Print($"Got index {idx} of {behaviors.Length}");
+        var behavior = behaviors[idx];
+        GD.Print($"Got behavior {behavior}");
+        return behavior;
+        // return behaviors[GD.Randi() % behaviors.Length];
+    }
+    public BehaviorMapping AnyNext() {
+        return Any(Next);
+    }
+
+    public BehaviorMapping AnyInterrupt() {
+        if (Interrupt is not null) return Any(Interrupt);
+        return null;
+    }
+
+    public override string ToString() {
+        return $"BehaviorMapping ({Name})";
+    }
+}
+
+public class BossPhase1Config : IBossBehaviorConfig {
+    public BehaviorMapping Initial {
+        get { GD.Print("yeah okay..."); GD.Print(BossPhase1Config.Wait); return BossPhase1Config.Wait; }
+    }
+    public static BehaviorMapping Shoot = new BehaviorMapping("BoringShoot", BossPhase1Config.Wait);
+    public static BehaviorMapping Wait = new BehaviorMapping(
+        "Wait",
+        new BehaviorMapping[]{BossPhase1Config.Shoot},
+        new BehaviorMapping[]{BossPhase1Config.Shoot}
+    );
+}
+
+public class BossManager : Node {
     public Boss DaBoss;
+    public IBossBehaviorConfig Config = new BossPhase1Config();
+    public BehaviorMapping ActiveBehavior;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         DaBoss = GetTree().Root.GetNode<Boss>("Game/Boss");
         GD.Print("Starting manager");
-        StartingBehavior = new Wait();
-        StartingBehavior.Done += Next;
-        StartingBehavior.Start(DaBoss);
+
+        ActiveBehavior = Config.Initial;
+        Run();
+    }
+
+    void Run() {
+        GD.Print("pls");
+        GD.Print(ActiveBehavior);
+        GD.Print($"Running a '{ActiveBehavior.Name}'");
+        var behavior = BossBehaviors.MakeA(ActiveBehavior.Name);
+        behavior.Done += Next;
+        behavior.Start(DaBoss);
     }
 
     void Next(object sender, BossBehaviorDoneArgs e) {
         GD.Print("In the next method wow ", e.Terminal);
-        if (StartingBehavior is Wait) {
-            StartingBehavior = new BoringShoot();
-        } else if (StartingBehavior is BoringShoot) {
-            StartingBehavior = new Wait();
-        }
 
-        StartingBehavior.Done += Next;
-        StartingBehavior.Start(DaBoss);
+        ActiveBehavior = ActiveBehavior.AnyNext();
+        Run();
     }
 }
