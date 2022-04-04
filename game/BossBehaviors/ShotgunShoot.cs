2cb4ffbd8fa8256d83a1e0af14af823a54789c0d
Michael Lange <dingoeatingfuzz@gmail.com>
2022-04-03 19:29:35 -0700

    Make the shotgun behavior more fun by choosing a rando gun to shoot

--- game/BossBehaviors/ShotgunShoot.cs
+++ game/BossBehaviors/ShotgunShoot.cs
@@ -13,12 +13,11 @@ public class ShotgunShoot : Node, IBossBehavior
         var count = 10;
 
         var guns = boss.Guns<BasicBossGun>();
-        foreach (var gun in guns) {
-            // TODO: Replace random angle with player angle to player position
-            float ang = (float)GD.RandRange(Mathf.Pi / 2, Mathf.Pi / 2 * 3);
-            foreach (var dir in Math.FanAngle(count, ang, Mathf.Pi/4)) {
-                gun.Shoot(dir);
-            }
+        var gun = guns[(int)(GD.Randi() % guns.Count)];
+        // TODO: Replace random angle with player angle to player position
+        float ang = (float)GD.RandRange(Mathf.Pi / 2, Mathf.Pi / 2 * 3);
+        foreach (var dir in Math.FanAngle(count, ang, Mathf.Pi/4)) {
+            gun.Shoot(dir);
         }
 
 
